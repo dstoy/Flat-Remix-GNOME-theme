b0dd84d1d47767acc6e261837a7b8b92e382598f
Dimitar Stoyanov <me@mitaka.me>
2017-12-24 21:27:40 +0000

    Added an active/hover state for the buttons in the modal dialog

--- sass/parse-sass.sh
+++ sass/parse-sass.sh
@@ -5,4 +5,6 @@ do
   sassc -a $scss ${scss%%.scss}.css
 done
 
+mv gnome-shell.css ../Flat\ Remix/gnome-shell/
+
 gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell --method org.gnome.Shell.Eval 'Main.loadTheme();'
